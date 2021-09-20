278274e5018d53649b7f5156bf2d14e623dc6646
Shalini M <mshalini5600@gmail.com>
2021-09-20 17:46:54 +0530

    commit
    commit

--- src/main/java/spring/boot/weather/forecasting/Client.java
+++ src/main/java/spring/boot/weather/forecasting/Client.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//main 
 @SpringBootApplication
 @EnableSwagger2
 public class Client {
